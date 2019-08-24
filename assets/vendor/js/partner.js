f76513cbf4bdb16673cff08f67e95a26bc5e01c0
tanvi111299 <tanvisharma110811@gmail.com>
2019-08-24 17:44:13 +0530

    index page re-added

--- assets/vendor/js/partner.js
+++ assets/vendor/js/partner.js
@@ -1,3 +1,10 @@
 	/**********************/
 	/*	Client carousel   */
 	/**********************/
+	$('.carousel-client').bxSlider({
+		auto: true,
+	    slideWidth: 470,
+	    minSlides: 2,
+	    maxSlides: 3,
+	    controls: false
+	});
